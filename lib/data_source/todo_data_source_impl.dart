46b6d3726e0969fcea663bc55ff87158e8941931
complete0415jiyoung <complete0415@naver.com>
2025-04-02 14:14:35 +0900

    FEAT: datasource 수정 by 이지영

--- lib/data_source/todo_data_source_impl.dart
+++ lib/data_source/todo_data_source_impl.dart
@@ -1,11 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:todo_app/data_source/todo_data_source.dart';
 
 class TodoDataSourceImpl implements TodoDataSource {
+  final filePath = '';
+
   @override
-  Future<void> readTodo(Map<String, dynamic> todo) {
-    try {} catch (e) {}
+  Future<void> readTodo(Map<String, dynamic> todo) async {
+    try {
+      final File jsonFile = File(filePath);
+      String jsonFromFile = await jsonFile.readAsString();
+
+      return jsonDecode(jsonFromFile);
+    } on FileSystemException {
+      throw FormatException('JSON 파싱오류');
+    } catch (e) {
+      print('예상치 못한 오류발생 :$e');
+    }
   }
 
   @override
-  Future<void> writeTodo(Map<String, dynamic> todo) {}
+  Future<void> writeTodo(List<Map<String, dynamic>> todos) {
+    // TODO: implement writeTodo
+    throw UnimplementedError();
+  }
 }
