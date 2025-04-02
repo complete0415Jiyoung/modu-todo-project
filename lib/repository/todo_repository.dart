3789fad87d2816f05153661d43e0b84e87e3c38b
Si Yujun <siuj0810@gmail.com>
2025-04-02 14:24:29 +0900

    fix: repository 수정

---
+++ lib/repository/todo_repository.dart
@@ -0,0 +1,9 @@
+import 'package:todo_app/model/todo.dart';
+
+abstract interface class TodoRepository {
+  Future<List<Todo>> getTodos();
+  Future<void> addTodo(String title);
+  Future<void> updateTodo(int id, String newTitle);
+  Future<void> toggleTodo(int id);
+  Future<void> deleteTodo(int id);
+}
