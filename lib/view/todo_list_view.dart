bc0700903c7d586b26464bf146ded7bfb56fa3da
Hwamok Lee <ghkahr1890@gmail.com>
2025-04-02 15:32:21 +0900

    feat: listview by이화목

--- lib/view/todo_list_view.dart
+++ lib/view/todo_list_view.dart
@@ -2,7 +2,12 @@ import 'package:todo_app/data_source/todo_data_source_impl.dart';
 import 'package:todo_app/repository/todo_repository.dart';
 import 'package:todo_app/repository/todo_repository_impl.dart';
 
-
+class TodoView{
+  final TodoRepository todoRepository = TodoRepositoryImpl (
+    TodoDataSourceImpl(),
+  );
+  todoRepository.getTodos();
+}
 class TodoListView{
 
   void listView() async{
